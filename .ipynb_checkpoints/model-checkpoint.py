a1afe853eae890eb757229394778a0e8569dc235
Vivian Mosomi <112128399+Vee2002@users.noreply.github.com>
2024-11-29 22:49:35 +0300

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/model-checkpoint.py
+++
@@ -1,6 +0,0 @@
- 
-X_train_resampled = joblib.load('X_train_resampled')
-X_test_encoded = joblib.load('X_test_encoded')
-y_train_resampled = joblib.load('y_train_resampled')
-y_test = joblib.load('y_test')
-lr = joblib.load('lr')
