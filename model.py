bc124696cf118199e9405465902a562c69674f5f
Vivian Mosomi <112128399+Vee2002@users.noreply.github.com>
2024-11-29 22:50:51 +0300

    Delete model.py

--- model.py
+++
@@ -1,9 +0,0 @@
- 
-import joblib
-X_train_resampled = joblib.load('X_train_resampled.pkl')
-X_test_encoded = joblib.load('X_test_encoded.pkl')
-y_test = joblib.load('y_test.pkl')
-lr = joblib.load('lr.pkl')
-
-# Making predictions
-predictions = lr.predict(X_test_encoded)
